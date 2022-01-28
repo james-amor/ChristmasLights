1a7fa46e0195a6e722704fb516d4f5c6c1e5c4e9
james-amor <71065154+james-amor@users.noreply.github.com>
2022-01-28 12:16:09 +0000

    Revert "Update BedroomWindowProcessor.ino"
    This reverts commit 79c8dbae5e771ab72137817ced87f857b9a3f50a.

--- BedroomWindow/BedroomWindowProcessor.ino
+++ BedroomWindow/BedroomWindowProcessor.ino
@@ -13,7 +13,6 @@
  */
 //===================================================================
 
-
 #include "PCA9685.h"
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -277,26 +276,8 @@ void receiveRFData ()
     messageReady = true;
   }
 
-  //Ensure that all channels are turned off when no data has been received for more than a second
-  if ((messageReady == false) && (lastMsgReceived > 0))
-  {
-    if ((millis() - lastMsgReceived) > 1000UL)
-    {
-      digitalWrite (RED_PIN, HIGH);
-      digitalWrite (GREEN_PIN, LOW);      
-
-      resetInBuffer();
-      bytesReceived = CONTROLLER_CHANNEL_COUNT;
-      processBuffer();
-      
-      lastMsgReceived = 0;
-    }
-  }
-
   if (messageReady == true)
-  {    
-    lastMsgReceived = millis();
-    
+  {      
     if (hadError)
     {
       digitalWrite (RED_PIN, (digitalRead(RED_PIN) == HIGH) ? LOW : HIGH);
@@ -330,10 +311,6 @@ void receiveRFData ()
       Serial.println ("E1");
       hadError = true;
     }
-    else if (messageReceived.dyntag == lastDyntag)
-    {
-      //This is a retransmit, and we handled the last message, so ignore it
-    }
     else if (lastDyntag > 0)
     {
       if (messageReceived.dyntag == 1)
@@ -359,7 +336,7 @@ void receiveRFData ()
     
     lastDyntag = messageReceived.dyntag;      
     messageReady = false;
-  } 
+  }      
 }
 //==================================================================================
 void receiveSerialData ()
